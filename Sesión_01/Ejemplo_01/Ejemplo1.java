739d000653dc197e7ffe63881ddce6bf890f0be4
Andrea Chavez Rubio <99062419+AndreaChavezR@users.noreply.github.com>
2025-05-06 19:32:28 -0600

    Add files via upload

---
+++ Sesión_01/Ejemplo_01/Ejemplo1.java
@@ -0,0 +1,7 @@
+public class Ejemplo1 {
+    public static void main(String[] args){
+        //Código de todo el programa
+        System.out.println("¡Hola mundo!");
+
+    }
+}
---
+++ Sesión_01/Ejemplo_02/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main (String[] args){
+        //Crear mis objetos
+        //1.Clase
+        //2.Identificador
+        //3.              new + constructor
+        Estudiante est1 = new Estudiante();
+        est1.nombre = "Sofia";
+        est1.edad = 15;
+
+        est1.saludar();
+
+    }
+}
